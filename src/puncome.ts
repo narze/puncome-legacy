616dfaaee7712c04d16e0276a93a05ad92729aa3
Manassarn Manoonchai <manassarn@gmail.com>
2019-09-09 20:17:51 +0700

    :construction: Puncome#process with exact match

--- src/puncome.ts
+++ src/puncome.ts
@@ -1,4 +1,16 @@
+import words from './tnc-1k-wordlist'
+
 export default class Puncome {
+  static process(input: string): Array<string> {
+    let lines = words
+
+    if (lines.indexOf(input) !== -1) {
+      return [];
+    } else {
+      return [input];
+    }
+  }
+
   static Sum(a: number, b: number): number {
     let c = a + b;
     return c;
--- test/puncome.test.ts
+++ test/puncome.test.ts
@@ -17,3 +17,22 @@ describe('wordcut', () => {
     expect(result).equal('สวัสดี|ชาว|โลก');
   });
 });
+
+describe('process', () => {
+  context('when the word is within 1k frequent list', () => {
+    it('returns empty array', () => {
+      let result = Puncome.process("ที่");
+
+      expect(result.length).to.eq(0);
+    })
+  })
+
+  context('when the word is not within 1k frequent list', () => {
+    it('returns unmatched words', () => {
+      let result = Puncome.process("ไดโนเสาร์");
+
+      expect(result.length).to.eq(1);
+      expect(result).to.include("ไดโนเสาร์");
+    })
+  })
+})
